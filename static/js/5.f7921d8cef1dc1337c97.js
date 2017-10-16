7910a2ca627b10020e973c0f16936e9c8136f89f
OXOYO <zmn2007.hi@163.com>
2017-10-16 20:43:00 +0800

    Update.

--- static/js/5.f7921d8cef1dc1337c97.js
+++ static/js/5.f7921d8cef1dc1337c97.js
@@ -1,2 +1,2 @@
 webpackJsonp([5],{Id8V:function(t,e,a){"use strict";Object.defineProperty(e,"__esModule",{value:!0}),e.default={data:{status:200,msg:"获取账号列表成功！",data:{count:1,list:[{id:1,account:"test",name:"test",type:1,status:1,create_time:"1506925466426",update_time:"1506925466426"}]}}}}});
-//# sourceMappingURL=5.78edb0ea3999b0004887.js.map
+//# sourceMappingURL=5.f7921d8cef1dc1337c97.js.map
